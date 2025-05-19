a8b3cf32845735f089863236bb3a52ce3af5ccb3
Pedronxw <pedroalmeidan@gmail.com>
2025-05-19 13:41:42 -0300

    fix: health check

--- src/infra/http/controllers/health/health.controller.ts
+++ src/infra/http/controllers/health/health.controller.ts
@@ -1,7 +1,9 @@
+import { Public } from '@/infra/auth/public';
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('health')
+@Public()
 @Controller('/')
 export class HealthController {
   constructor(
